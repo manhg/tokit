62f84a7936aa711df73e9363b7604072000933ca
Giang Manh <manhg@users.noreply.github.com>
2017-01-30 15:55:10 +0900

    Run on Py 3.6

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         ]
     },
     classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6'
     ]
 )
