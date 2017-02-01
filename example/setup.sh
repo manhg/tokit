fe2b0eb52858b55f03cd99fa5bb3876298d444f1
Giang Manh <manhg@users.noreply.github.com>
2017-02-01 17:54:45 +0900

    Improve docker

---
+++ example/setup.sh
@@ -0,0 +1,29 @@
+set -ex
+
+VER='3.6.0'
+LANG="C.UTF-8"
+
+DEV=' gcc make libreadline-dev ncurses-dev libssl-dev zlib1g-dev libpq-dev libev-dev libsass-dev'
+TOOLS='nodejs wget vim ca-certificates'
+
+apt-get update > /dev/null
+
+apt-get install -y \
+    $DEV $TOOLS --no-install-recommends > /dev/null
+
+cd /tmp
+wget --no-verbose --no-check-certificate \
+        https://www.python.org/ftp/python/$VER/Python-$VER.tgz
+
+tar xzf Python-$VER.tgz
+cd Python-$VER
+./configure  > /dev/null
+make  > /dev/null
+make install  > /dev/null
+pip3 install --no-cache-dir -r /tmp/requirements.txt
+
+rm -rf /tmp/Python-$VER
+rm -rf /var/lib/apt/lists/*
+apt-get -y remove $DEV
+
+chmod 755 /usr/bin/wait-for-it.sh
