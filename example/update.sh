170ace1d7e81e950179e35ecfffa80d00c89ff22
Giang Manh <manhg@users.noreply.github.com>
2017-02-05 07:01:14 +0900

    Only two docker compose sample

--- example/update.sh
+++ example/update.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+TOKIT_VER=${TOKIT_VER:=master}
+TOKIT_PORT=${TOKIT_PORT:=9000}
+TOKIT_ENV=${TOKIT_PORT:=docker}
+
 pip3 install --upgrade https://github.com/manhgd/tokit/archive/$TOKIT_VER.tar.gz
-/usr/local/bin/python3 /app/src/app.py --host=0.0.0.0 --port=$PORT --env=docker
+
+/usr/local/bin/python3 /app/src/app.py --host=0.0.0.0 \
+    --port=$TOKIT_PORT --env=$TOKIT_ENV
