4ff40711c0d9277ce83c7ede00561230abaa6604
Giang Manh <manhg@users.noreply.github.com>
2017-02-06 11:13:26 +0900

    split dockerfile

--- example/setup.sh
+++ example/setup.sh
@@ -5,8 +5,7 @@ adduser --system --no-create-home --disabled-password --disabled-login --shell /
 VER='3.6.0'
 LANG="C.UTF-8"
 
-DEV='git curl cmake gcc make libreadline-dev ncurses-dev libssl-dev'
-DEV="$DEV zlib1g-dev libpq-dev libev-dev libsass-dev libbz2-dev libsqlite3-dev"
+DEV='git curl cmake gcc make libreadline-dev ncurses-dev libssl-dev zlib1g-dev libpq-dev libev-dev libsass-dev libbz2-dev libsqlite3-dev'
 TOOLS='nodejs wget vim ca-certificates'
 
 apt-get update > /dev/null
@@ -25,6 +24,4 @@ make  > /dev/null
 make install  > /dev/null
 
 rm -rf /var/lib/apt/lists/*
-rm Python-$VER
-
-chmod 755 /usr/bin/wait-for-it.sh
+rm -rf /tmp/Python-*
