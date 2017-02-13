9d47edc429365c416690dcaa1c8efa2f68fc9486
Giang Manh <manhg@users.noreply.github.com>
2017-02-13 16:09:12 +0900

    Use Pypi..

--- example/update.sh
+++ example/update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-TOKIT_VER=${TOKIT_VER:=master}
+TOKIT_VER=${TOKIT_VER:=0.7}
 TOKIT_PORT=${TOKIT_PORT:=9000}
 TOKIT_ENV=${TOKIT_PORT:=docker}
 
-pip3 install --upgrade https://github.com/manhgd/tokit/archive/$TOKIT_VER.tar.gz
+pip3 install --upgrade tokit==$TOKIT_VER
 
 /usr/local/bin/python3 /app/src/app.py --host=0.0.0.0 \
     --port=$TOKIT_PORT --env=$TOKIT_ENV
