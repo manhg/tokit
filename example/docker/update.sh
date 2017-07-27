396457db820f2b82ddc1e62bf8c8e102608d145c
Giang Manh <manhg@users.noreply.github.com>
2017-07-27 17:15:19 +0900

    Fix paths

--- example/docker/update.sh
+++ example/docker/update.sh
@@ -2,7 +2,7 @@
 TOKIT_PORT=${TOKIT_PORT:=9000}
 TOKIT_ENV=${TOKIT_PORT:=docker}
 
-pip3 install --upgrade /app/src/requirements.txt
+pip3 install --upgrade -r /app/src/requirements.txt
 
 /usr/local/bin/python3 /app/src/app.py \
     --host=0.0.0.0 \
