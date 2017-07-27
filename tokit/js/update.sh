ec5ed1377ab387b2099701b2a232e8e51e4c460d
Giang Manh <manhg@users.noreply.github.com>
2017-07-27 17:15:33 +0900

    Update riotjs

--- tokit/js/update.sh
+++ tokit/js/update.sh
@@ -9,10 +9,10 @@ curl http://coffeescript.org/v1/browser-compiler/coffee-script.js -s \
 curl https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.22.1/babel.min.js -s \
     -o babel.js
 
-curl https://raw.githubusercontent.com/riot/riot/v3.2.1/riot%2Bcompiler.min.js -s \
+curl https://raw.githubusercontent.com/riot/riot/v3.6.1/riot%2Bcompiler.min.js -s \
     -o riot-compiler.js
 
-curl https://raw.githubusercontent.com/riot/riot/v3.2.1/riot.min.js -s \
+curl https://raw.githubusercontent.com/riot/riot/v3.6.1/riot.min.js -s \
     -o ../../example/src/riot.js
 
 echo "Done"
